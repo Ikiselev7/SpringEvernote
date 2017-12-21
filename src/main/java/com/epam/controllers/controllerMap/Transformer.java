524dc0cc21e9a03b4296e42f99db5b94cb9fd39b
Ikiselev7 <ikiselev7@gmail.com>
2017-12-21 11:26:56 +0300

    transformers

--- src/main/java/com/epam/controllers/controllerMap/Transformer.java
+++ src/main/java/com/epam/controllers/controllerMap/Transformer.java
@@ -1,6 +1,13 @@
 package com.epam.controllers.controllerMap;
 
+/**
+ * Basic transformer interface.
+ *
+ * @param <S> Dto model
+ * @param <T> Controller model
+ */
 public interface Transformer<S, T> {
+
 
     S bind(T t);
 
